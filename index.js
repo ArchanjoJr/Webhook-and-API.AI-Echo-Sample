b5a4aec5794e63705315191af66b50bba8692f14
ArchanjoJr <math.calazans56@gmail.com>
2018-05-30 15:52:43 -0300

    add water fact

--- index.js
+++ index.js
@@ -78,6 +78,72 @@ restService.post('/test', (req, res) => {
           source: 'api-test-dial'
         })
       break;
+    case 'WaterFactIntent':
+    const opt = [
+      "Less than 1% of the water supply on land can be used as potable water,",
+      "At the moment a person feels thirsty, their body has lost more than 1% of their total amount of water.",
+      "Approximately 25,700 liters (6,800 gallons) of water are needed to grow one-day food for a family of four.",
+      "Underground water can take a human life only to cross a kilometer.",
+       "A person can live about a month without food, but only about a week without water.",                "If a human does not absorb enough water, dehydration is the result.",
+      "Most of the Earth's surface is permanently frozen or salted.",
+      "If all the water in the world were in one gallon, the fresh water available for use would be equal to only one tablespoon.",
+      "When water contains a lot of calcium and magnesium, it's called hard water.",
+      "Hard water is not suitable for all purposes for which water is normally used.",
+      "One acre of corn will produce 15,000 liters (4,000 gallons) of water a day in evaporation.",
+      "A small drip from a faucet can waste up to 75 liters of water a day.",
+      "Of all the earth's water, only 2.5% is fresh water.",
+      "Freshwater is groundwater (0.5%) or easily accessible water in lakes, streams, rivers, etc. (0.01%).",
+      "The oceans are very wide and there are many on earth, the oceans store most of the earth's water.",
+      "The oceans are apparently 97% of the total amount of water on Earth, 2% of which is frozen.",
+      "Human bones are 25% water.",
+      "Most people in the world should walk for at least 3 hours to get water.",
+      "Today, at least 400 million people live in regions with severe water shortages.",
+      "It takes 450 liters (120 gallons) of water to produce an egg.",
+      "Bottled water can be up to 1,000 times more expensive than tap water and may not be as safe.",
+      "Two-thirds of the water contained in a house is used in the bathroom.",
+      "Less than 1% of the water treated by public water is used for drinking and cooking.",
+      "More than 2 billion people on earth do not have a safe supply of water.",
+      "The best-selling brand of bottled water (Aquafina) is treated with Pepsi-packaged tap water.",
+      "70% percent of an elephant is water,",
+      "Ground water supplies serve about 80% of the population, while up to 4% of usable groundwater is already polluted.",
+      "At the moment a person feels thirsty, their body has lost more than 1% of their total amount of water.",
+      "Every day, it rains heavily in the United States to cover the entire state of Vermont with 2 feet of water,",
+      "Every day, US water users draw enough water to fill a line of Olympic size swimming pools that arrive around the world.",
+      "Water represents 70% of body mass.",
+      "You should drink enough to equal your body weight in ounces every day.If you weigh 200 pounds, drink 100 ounces.",
+      "It takes 7,000 liters (1,850 gallons) of water to refine a barrel of crude oil.",
+      "For every six ounces of caffeine or alcohol you consume, an additional 10 to 12 ounces of water is needed to rehydrate you.",
+      "We lost more than 2 liters of water every day through the normal vapor exchange of our skin, also known as sweating.",
+      "Insufficient water intake is a risk factor for colon, breast, urinary tract, kidney, bladder, prostate and testis cancers.",
+      "Hydration is critical to the bloodstream, allowing the immune system to reach the damaged tissues in greater numbers.",
+      "Did you know that 25% of the bottled water in the market is extracted from municipal faucets?",
+      "Pure water (the hydrogen and oxygen atoms) has a neutral pH of 7, which is neither acid nor basic.",
+      "Four gallons (1 gallon) of gasoline can contaminate approximately 2.8 million gallons (750,000 gallons) of water.",
+      "If all new sources of contamination could be eliminated in 10 years, 98% of all available groundwater would be free of pollution.",
+      "There are 12,000 different toxic chemical compounds in industrial use today, and more than 500 new chemicals are developed every year.",
+      "Water dissolves more substances than any other liquid.",
+      "Wherever water travels, it carries chemicals, minerals, and nutrients.",
+      "Pure water (the hydrogen and oxygen atoms) has a neutral pH of 7, which is neither acid nor basic.",
+      "Four gallons (1 gallon) of gasoline can contaminate approximately 2.8 million gallons (750,000 gallons) of water.",
+      "If all new sources of contamination could be eliminated in 10 years, 98% of all available groundwater would be free of pollution.",
+      "There are 12,000 different toxic chemical compounds in industrial use today, and more than 500 new chemicals are developed every year.",
+      "Water dissolves more substances than any other liquid,",
+      "Wherever water travels, it carries chemicals, minerals, and nutrients.",
+      "Freshwater animals are disappearing five times faster than terrestrial animals.",
+      "Every day the sun evaporates trillions of tons of water.",
+      "The weight that a person loses directly after intense physical activity is the weight of water, not fat,",
+      "It takes 5,680 liters (1,500 gallons) of water to process a barrel of beer.",
+      "At birth, water accounts for approximately 80% of children's body weight,",
+      "To process a chicken, we need 44 liters (11.6 gallons) of water.",
+      "To process one of the fruits or vegetables, we need 35 liters (9.3 gallons) of water.",
+    ];
+    const item = opt[Math.floor(Math.random()*options.length)]; 
+    res.status(200).json({
+      speech:item,
+      displayText:item,
+      source: 'api-test-dial',
+    });
+      break;
   }
 });
 
