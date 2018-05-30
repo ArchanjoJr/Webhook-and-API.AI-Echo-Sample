212dc0d9f49f2496af30da0e7b36c9d4c2d7eca4
ArchanjoJr <math.calazans56@gmail.com>
2018-05-30 12:38:59 -0300

    add simple test

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+let url = 'https://tecsus.com.br/tecmeter/service/call/json/medida/561';
+var options = {
+    url: url,
+    headers: {
+        "Authorization": "Bearer eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9.eyJpYXQiOiAxNTExODc1NzcwLjAsICJ1c2VyX2dyb3VwcyI6IHt9LCAidXNlciI6IHsiZmlyc3RfbmFtZSI6ICJTcHJpbmciLCAibGFzdF9uYW1lIjogIkZpZWxkMDkiLCAiZmlyc3RfYWMiOiBmYWxzZSwgInJlZ2lzdHJhdGlvbl9pZCI6ICIiLCAiaWQiOiAyMzMsICJyZXNldF9wYXNzd29yZF9rZXkiOiAiIiwgInJlc3BvbnNhdmVsIjogZmFsc2UsICJhZG0iOiBmYWxzZSwgInJlZ2lzdHJhdGlvbl9rZXkiOiAiIiwgImVtYWlsIjogInNwcmluZzlAdGVjc3VzLmNvbS5iciJ9LCAiZXhwIjogMTUxMTg4MzI3MC4wLCAiaG1hY19rZXkiOiAiMzU5YjYwYjMtZGY2ZS00NTE3LTgyZGItYzA5MDQ1ZTgxNjk3In0.LQ1DbMVBMWYsWeVn-BiWaK0DCkSwh_HxjANOmCFMhcs"
+    }
+};
 
 const restService = express();
 
@@ -24,6 +32,23 @@ restService.post("/echo", function(req, res) {
     speech: speech,
     displayText: speech,
     source: "webhook-echo-sample"
+  });
+});
+restService.post('/test', (req, res) => {
+  request(options, function (err, response, body) {
+    if(err){
+      console.log('err:', err);
+      res.status(400).send('ERROR');
+    } else {
+      console.log('body:', body);
+      var json = JSON.parse(body);
+      return res.status(200).json({
+        speech:`o valor final da sua conta é ${json['dados']['valor_final']}`,
+        displayText:`o valor final da sua conta é ${json['dados']['valor_final']}`,
+        source: 'webhooke-echo-sample',
+      })
+      
+    }
   });
 });
 
